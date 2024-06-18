597c1c41d8f40e947c38d22ce6726233b725595e
saul.castro <saul.castro@wizeline.com>
2024-06-18 14:31:00 -0600

    add class to reload hive partititon in athena

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-""" 
-Created by saul ramirez at 9/24/2020
-Updated by Saul Ramirez at 27/4/2021
-
-"""
-
-from reload_athena import Reload
+from reload_athena import ReloadHivePartition
 
 
 def lambda_handler(event, context):
@@ -14,8 +7,8 @@ def lambda_handler(event, context):
 
     if event.get('prefix'):
         prefix_event = event.get('prefix')
-        athena_reload = Reload(prefix=prefix_event)
+        athena_reload = ReloadHivePartition(prefix=prefix_event)
         athena_reload.reload()
     else:
-        athena_reload = Reload()
+        athena_reload = ReloadHivePartition()
         athena_reload.reload()
